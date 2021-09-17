0b5a8ceed7ee99fcb23d90072e10ecb2aeeae05b
zontaee <whdlsxo123@naver.com>
2021-09-17 09:37:07 +0900

    CH1 09.17 - Cho in tae

---
+++ ch01/src/ch01/Calculator.java
@@ -0,0 +1,16 @@
+package ch01;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		 int result = 10 + 20;
+		 // Alt + 위/아래 화살표
+		 System.out.println(result);
+		// 한줄 주석
+		 /*
+		  주석은 여러줄 
+		   가능합니다 
+		 */
+	}
+
+}
---
+++ ch01/src/ch02/semicolonExam.java
@@ -0,0 +1,16 @@
+package ch02;
+
+public class semicolonExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int x = 1;  // 변수 x를 선언하고 정수 1을 저장
+      int y = 2;  // 변수 y를 선언하고 정수 2를 저장
+      int result = x + y; // 변수 result를 선언하고 변수 x와 y를 더한 값을 저장
+      
+      // 출력기능의 라이브러리를 제공함 - println()함수(메소드)
+      System.out.println(result); // 콘솔에 출력하는 메소드 호출
+      
+	}
+
+}
